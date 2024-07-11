c3c52d4a3b99ca92cfe408df495c67ebb2ab41d7
Mar7155 <mlm711105@gmail.com>
2024-07-11 00:11:05 -0600

    Add image preview and fix build app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mar7155.github.io'
+  site: 'https://localhost:4321',
+  integrations: [sitemap()]
 });

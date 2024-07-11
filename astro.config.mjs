3266de94f2a733d7b03a18cb14c1ea71fb641e41
Mar7155 <mlm711105@gmail.com>
2024-07-11 15:58:32 -0600

    add out dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  
+  out: 'dist',
   site: 'https://mar7155.github.io/Portfolio',
   integrations: [sitemap()]
 });
